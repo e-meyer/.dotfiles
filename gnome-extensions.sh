fa84ca4f0e937a3965f4da6e21ad6344fd6b5af9
e-meyer <edmundomeyer01@gmail.com>
2023-06-27 18:34:05 -0300

    gnome-extensions.sh

---
+++ gnome-extensions.sh
@@ -0,0 +1,5 @@
+sudo apt install gnome-shell-extensions
+sudo apt install gnome-shell-extension-manager
+sudo apt-get install gnome-shell-extension-tool
+gnome-shell-extension-tool -e dash-to-dock@micxgx.gmail.com
+
